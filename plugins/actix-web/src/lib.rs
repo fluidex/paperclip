e099796d2453ae3a10d98fd5a5a63135125d785d
Tiago Castro <tiagolobocastro@gmail.com>
2021-11-07 16:58:44 +0000

    fix(paperclip-actix): add symlink to swagger-ui
    This way it will be uploaded to crates.io

--- plugins/actix-web/src/lib.rs
+++ plugins/actix-web/src/lib.rs
@@ -50,7 +50,7 @@ pub struct App<T, B> {
 }
 
 #[cfg(feature = "swagger-ui")]
-static SWAGGER_DIST: Dir = include_dir!("../swagger-ui/dist");
+static SWAGGER_DIST: Dir = include_dir!("./swagger-ui/dist");
 
 /// Extension trait for actix-web applications.
 pub trait OpenApiExt<T, B> {
